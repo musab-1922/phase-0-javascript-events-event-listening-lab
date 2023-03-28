87f7867d514e92131e0b2010e953c79611e0182a
musab <musababdi1922@gmail.com>
2023-03-28 08:49:37 +0300

    Completed Assignement

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 function addingEventListener() {
 }
+
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
+
+    function addingEventListener() {
+
+      const input = document.getElementById('button');
+  
+      function clickAlert() {
+        alert('I was clicked!');
+      }
+  
+      input.addEventListener('click', clickAlert);
+  
+  }
